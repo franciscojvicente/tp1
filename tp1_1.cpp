f646b08d9c898d6b96b13fe66238352a0b653276
franv <fran.vicente03@gmail.com>
2023-03-16 18:08:14 -0300

    add work with pointer

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,5 +2,20 @@
 
 int main () {
     printf("Hola mundo!");
+
+    int var = 5; 
+    int *pVar = &var;
+
+    printf("\nEl contenido del puntero es: %d", *pVar);
+
+    printf("\nLa direccion de memoria almacenada por el puntero es: %d", pVar);
+
+    printf("\nLa direccion de memoria de la variable es: %d", &var);
+
+    printf("\nLa direccion de memoria del puntero es: %d", &pVar);
+
+    printf("\nEl tamaño de memoria usado por la variable es: %d", sizeof(var));
+
     return 0;
 }
+
